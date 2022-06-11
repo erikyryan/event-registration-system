cdd2dd900763ff04799219142c96c8c2118aa76b
erikyryan <erikyryang@gmail.com>
2022-06-11 18:19:19 -0300

    Update SpringBootApplication

--- backend/asimo/v/src/main/java/asimo/v/VApplication.java
+++ backend/asimo/v/src/main/java/asimo/v/VApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
 import java.util.Date;
 
 @SpringBootApplication
