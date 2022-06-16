7b9e9919e6b3bfcaa9309d964f50b7feba2bae7c
erikyryan <erikyryang@gmail.com>
2022-06-16 11:27:15 -0300

    adding token to user

--- backend/asimo/v/src/main/java/asimo/v/controllers/UserController.java
+++ backend/asimo/v/src/main/java/asimo/v/controllers/UserController.java
@@ -3,6 +3,7 @@ package asimo.v.controllers;
 import asimo.v.entities.User;
 import asimo.v.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,23 +14,23 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id) {
+    @GetMapping(name = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> findById(@PathVariable Long id, @RequestHeader("token") String token) {
         return service.findById(id);
     }
 
-    @PostMapping
-    public ResponseEntity<User> save(@RequestBody User user){
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> save(@RequestBody User user, @RequestHeader("token") String token){
         return service.save(user);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<User> update(@PathVariable Long id,@RequestBody User user){
+    @PutMapping(name = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> update(@PathVariable Long id,@RequestBody User user, @RequestHeader("token") String token){
         return service.update(id,user);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<User> delete(@PathVariable Long id){
+    @DeleteMapping(name = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> delete(@PathVariable Long id, @RequestHeader("token") String token){
         return service.delete(id);
     }
 
