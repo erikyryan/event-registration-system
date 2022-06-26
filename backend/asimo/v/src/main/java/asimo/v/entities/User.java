ca73c472d76b42a39436d778af82450fce75af55
erikyryan <erikyryang@gmail.com>
2022-06-26 15:41:18 -0300

    Bugfix/Fix Repository, controller and service user

--- backend/asimo/v/src/main/java/asimo/v/entities/User.java
+++ backend/asimo/v/src/main/java/asimo/v/entities/User.java
@@ -3,15 +3,7 @@ package asimo.v.entities;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
@@ -50,8 +42,7 @@ public class User{
     private String password; 
 
     private String salt; 
-    
-    @NotNull
+
     @Column(name="dtnascimento")
     @Temporal(TemporalType.TIMESTAMP)
     private Date birthDate;
@@ -62,7 +53,10 @@ public class User{
     @OneToMany(mappedBy = "user")
     private List<Sale> tickets;
 
-    //Getters and Setters
+	public User() {
+
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -159,7 +153,7 @@ public class User{
 		this.salt = salt;
 	}
 
-	public User(final User user) {
+	public User(final UserObject user) {
 		this.name = user.getName();
 		this.doc = user.getDoc();
 		this.sex = user.getSex();
@@ -174,5 +168,18 @@ public class User{
 		this.salt = UserService.generateSalt();
 		this.password = UserService.encryptPassword(password, salt);
 	}
-	
+
+	@Override
+	public String toString() {
+		return "name='" + name + '\'' +
+				", doc='" + doc + '\'' +
+				", sex='" + sex + '\'' +
+				", email='" + email + '\'' +
+				", telephone='" + telephone + '\'' +
+				", login='" + login + '\'' +
+				", password='" + password + '\'' +
+				", birthDate=" + birthDate +
+				", role=" + role +
+				", tickets=" + tickets;
+	}
 }
