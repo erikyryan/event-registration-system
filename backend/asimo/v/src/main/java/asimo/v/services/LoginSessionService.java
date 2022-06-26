d0e9cb233ea98ec82981e7e3dabe3e597c89264e
erikyryan <erikyryang@gmail.com>
2022-06-26 15:41:35 -0300

    Bugfix/Fix LoginSession

--- backend/asimo/v/src/main/java/asimo/v/services/LoginSessionService.java
+++ backend/asimo/v/src/main/java/asimo/v/services/LoginSessionService.java
@@ -5,12 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import asimo.v.entities.LoginSession;
 import asimo.v.entities.User;
 import asimo.v.repositories.LoginSessionRepository;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LoginSessionService {
 
-	@Autowired
-	LoginSessionRepository loginSessionRepository;
-	
+	private LoginSessionRepository loginSessionRepository;
+
+	public LoginSessionService(@Lazy LoginSessionRepository loginSessionRepository){
+		this.loginSessionRepository = loginSessionRepository;
+	}
+
+
 	public String generateSession(User userS) {
 		LoginSession userSession = new LoginSession(userS);
 		loginSessionRepository.save(userSession);
