276bd9e2419cb7f2c9fae4a294a36d5817423970
matews-sousa <mateussdamiano@gmail.com>
2022-07-01 17:03:05 -0300

    added admin pages with dashboard layout and added more custom themes

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+import React from "react";
+import DashboardLayout from "../components/Dashboard/DashboardLayout";
+
+const Dashboard = () => {
+  return <DashboardLayout>Dashboard content</DashboardLayout>;
+};
+
+export default Dashboard;
