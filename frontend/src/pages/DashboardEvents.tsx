50c89c83133acc92e5822ddc1cd51acbb0f9b8fd
matews-sousa <mateussdamiano@gmail.com>
2022-07-01 17:03:05 -0300

    added admin pages with dashboard layout and added more custom themes

---
+++ frontend/src/pages/DashboardEvents.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DashboardLayout from '../components/Dashboard/DashboardLayout'
+
+const DashboardEvents = () => {
+  return (
+    <DashboardLayout>DashboardEvents</DashboardLayout>
+  )
+}
+
+export default DashboardEvents
