423383a6db0b9f37ecc081f6c3fb2dd38d834ce2
matews-sousa <mateussdamiano@gmail.com>
2022-07-02 14:52:17 -0300

    added users list component

---
+++ frontend/src/components/UsersList.tsx
@@ -0,0 +1,69 @@
+import {
+  Box,
+  Typography,
+  Button,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody
+} from "@mui/material";
+import { IUser } from "../types/IUser";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+interface Props {
+  users: IUser[];
+}
+
+const UsersList = ({ users }: Props) => {
+  return (
+    <Box sx={{ minWidth: 1050 }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Nome</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Documento</TableCell>
+            <TableCell>Telefone</TableCell>
+            <TableCell>Nascimento</TableCell>
+            <TableCell>Função</TableCell>
+            <TableCell>Ações</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.slice(0, 10).map((user) => (
+            <TableRow hover key={user.id}>
+              <TableCell>
+                <Box
+                  sx={{
+                    alignItems: "center",
+                    display: "flex"
+                  }}>
+                  <Typography color="textPrimary" variant="body1">
+                    {user.name}
+                  </Typography>
+                </Box>
+              </TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.doc}</TableCell>
+              <TableCell>{user.phone}</TableCell>
+              <TableCell>{user.birthDate}</TableCell>
+              <TableCell>{user.role}</TableCell>
+              <TableCell>
+                <Button startIcon={<EditIcon />} size="small" variant="outlined" sx={{ mr: 1 }}>
+                  Editar
+                </Button>
+                <Button startIcon={<DeleteIcon />} size="small" variant="outlined" color="error">
+                  Deletar
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Box>
+  );
+};
+
+export default UsersList;
--- frontend/src/pages/Users.tsx
+++ frontend/src/pages/Users.tsx
@@ -1,8 +1,88 @@
 import React from "react";
+import { v4 as uuid } from "uuid";
 import DashboardLayout from "../components/Dashboard/DashboardLayout";
+import { Box, Typography, Button } from "@mui/material";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <DashboardLayout>Users</DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "100%",
+          mb: 3
+        }}>
+        <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+          Usuários
+        </Typography>
+        <Box>
+          <Button variant="contained">Add Usuário</Button>
+        </Box>
+      </Box>
+
+      <UsersList users={users} />
+    </DashboardLayout>
+  );
 };
 
 export default Users;
+
+const users = [
+  {
+    id: uuid(),
+    name: "Ekaterina Tankova",
+    email: "ekaterina.tankova@devias.io",
+    doc: "999.999.999-99",
+    birthDate: 1555016400000,
+    phone: "304-428-3097",
+    role: "USER"
+  },
+  {
+    id: uuid(),
+    doc: "999.999.999-99",
+    birthDate: 1555016400000,
+    email: "cao.yu@devias.io",
+    name: "Cao Yu",
+    phone: "712-351-5711",
+    role: "FUNC"
+  },
+  {
+    id: uuid(),
+    doc: "999.999.999-99",
+    birthDate: 1555016400000,
+    email: "mateussdamiano@gmail.com",
+    name: "Mateus Sousa",
+    phone: "712-351-5711",
+    role: "FUNC"
+  },
+  {
+    id: uuid(),
+    doc: "999.999.999-99",
+    birthDate: 1555016400000,
+    email: "cao.yu@devias.io",
+    name: "Cao Yu",
+    phone: "712-351-5711",
+    role: "ADMIN"
+  },
+  {
+    id: uuid(),
+    doc: "999.999.999-99",
+    birthDate: 1555016400000,
+    email: "cao.yu@devias.io",
+    name: "Cao Yu",
+    phone: "712-351-5711",
+    role: "USER"
+  },
+  {
+    id: uuid(),
+    doc: "999.999.999-99",
+    birthDate: 1555016400000,
+    email: "cao.yu@devias.io",
+    name: "Cao Yu",
+    phone: "712-351-5711",
+    role: "USER"
+  }
+];
