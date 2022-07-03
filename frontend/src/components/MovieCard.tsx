e34bbfafa8b70eda6e698a19e9bfc9d4cbead61e
matews-sousa <mateussdamiano@gmail.com>
2022-07-03 11:23:33 -0300

    created Movies and AddMovie pages for admin dashboard

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import { Button, Card, CardActions, CardContent, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 interface Props {
   event: {
@@ -10,16 +11,24 @@ interface Props {
 
 const MovieCard = ({ event }: Props) => {
   return (
-    <Card sx={{ marginBottom: 2 }}>
+    <Card sx={{ mb: 2 }}>
       <CardContent>
-        <Typography variant="h4">{event.title}</Typography>
+        <Typography variant="h5" align="center">
+          {event.title}
+        </Typography>
         <Typography variant="body2" sx={{ marginTop: 2 }}>
           {event.description}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" component={Link} to="/event" variant="contained">
-          View Details
+        <Button
+          size="small"
+          component={Link}
+          endIcon={<ArrowForwardIcon />}
+          to="/event"
+          variant="outlined"
+          color="primary">
+          Ver sessões
         </Button>
       </CardActions>
     </Card>
