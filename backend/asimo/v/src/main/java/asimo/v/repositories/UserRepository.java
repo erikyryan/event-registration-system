b50725ebad578e4de9c49f0f2367e845c3c38108
Davi <daviifernandes06@gmail.com>
2022-07-03 22:27:33 -0300

    remoção do atributo login.

--- backend/asimo/v/src/main/java/asimo/v/repositories/UserRepository.java
+++ backend/asimo/v/src/main/java/asimo/v/repositories/UserRepository.java
@@ -10,8 +10,6 @@ import asimo.v.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
-	Optional<User> findByLogin(String login);
-
 	Optional<User> findByEmail(String email);
 	
 	Optional<User> findByUserIdentifier(String identifier);
