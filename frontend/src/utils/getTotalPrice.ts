7b50c49c5162527c0113a3839337f517a4656741
matews-sousa <mateussdamiano@gmail.com>
2022-07-12 12:09:07 -0300

    remove seat selection when close or reload page and fix date displaying

--- frontend/src/utils/getTotalPrice.ts
+++ frontend/src/utils/getTotalPrice.ts
@@ -1,11 +1,13 @@
 import { ISelectedSeat } from "../types/ISeat";
 
-const getTotalPrice = (selected?: ISelectedSeat[]) => {
+const getTotalPrice = (selected?: ISelectedSeat[], currentUserRole?: string) => {
   const totalPrice = selected?.reduce((prev, curr) => {
     return prev + curr.price;
   }, 0);
 
-  return totalPrice;
+  if (!totalPrice) return;
+
+  return currentUserRole !== "USER" ? (totalPrice * 1.05).toFixed(2) : totalPrice;
 };
 
 export default getTotalPrice;
